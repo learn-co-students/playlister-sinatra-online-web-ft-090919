c0238856f7e8511f039e440f60deeb4c1ebab6b4
clonex10100 <clonex10100@gmail.com>
2019-11-09 17:43:40 -0500

    Done.

---
+++ app/models/concerns/slugify.rb
@@ -0,0 +1,12 @@
+module Slugify
+  module InstanceMethods
+    def slug
+      self.name.downcase.gsub(" ", "-")
+    end
+  end
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find { |a| a.slug == slug }
+    end
+  end
+end
